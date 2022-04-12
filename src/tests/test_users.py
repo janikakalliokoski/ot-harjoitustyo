42c92117a710dd858cec223f6e5a83af7a140f19
janika.kalliokoski@helsinki.fi <jbkallio@cubbli20-gold.local>
2022-04-12 12:26:42 +0300

    coverage

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -1,28 +1,28 @@
 import unittest
 
-from repositories.user_repository import user_repository
+from repositories.user_repository import User_repository
 from entities.user import User
 
 
 class TestUserRepository(unittest.TestCase):
     def setUp(self):
-        user_repository.delete_users()
+        User_repository.delete_users()
         self.user1 = User('janika', 'abc123')
         self.user2 = User('isabel', 'isabel123')
 
     def test_create(self):
-        user_repository.create_user(self.user1)
-        all = user_repository.find_users()
+        User_repository.create_user(self.user1)
+        all = User_repository.find_users()
         self.assertEqual(all[0].username, self.user1.username)
 
     def test_find_users(self):
-        user_repository.create_user(self.user1)
-        user_repository.create_user(self.user2)
-        all = user_repository.find_users()
+        User_repository.create_user(self.user1)
+        User_repository.create_user(self.user2)
+        all = User_repository.find_users()
         self.assertEqual(all[0].username, self.user1.username)
         self.assertEqual(all[1].username, self.user2.username)
 
     def test_find_by_username(self):
-        user_repository.create_user(self.user2)
-        user = user_repository.find_by_username(self.user2.username)
+        User_repository.create_user(self.user2)
+        user = User_repository.find_by_username(self.user2.username)
         self.assertEqual(user.username, self.user2.username)
